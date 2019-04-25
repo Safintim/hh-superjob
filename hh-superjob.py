efd4ba54542ac5331b9f0329add958506be7d9cd
Safintim <Sarami96>
2019-04-25 11:50:36 +0300

    refactoring

--- hh-superjob.py
+++ hh-superjob.py
@@ -61,8 +61,8 @@ def provide_params_sj(text=''):
     }
 
 
-def create_dict_language(language, vac_found, vac_processed, avg_salary):
-    dict_result = {
+def create_dict_language_statistics(language, vac_found, vac_processed, avg_salary):
+    return {
         language: {
             'vacancies_found': vac_found,
             'vacancies_processed': vac_processed,
@@ -70,26 +70,22 @@ def create_dict_language(language, vac_found, vac_processed, avg_salary):
         }
     }
 
-    return dict_result
-
 
 def get_statistics_language_hh(api_url, language):
 
-    vacancies_found = 0.
+    vacancies_found = 0
     all_salaries_vacancies = []
 
     page = 0
     number_pages = 1
     while page < number_pages:
-        print(page, language)
         response = requests.get(api_url, params=provide_params_hh(page, language)).json()
         number_pages = response['pages']
         page += 1
         vacancies_language = response['items']
+        vacancies_found = response['found']
 
         all_salaries_vacancies.extend(calculation_salaries_vacancies_hh(vacancies_language))
-
-        vacancies_found = response['found']
 
     is_salary = list(filter(lambda x: x is not None, all_salaries_vacancies))
     vacancies_processed = len(is_salary)
@@ -98,20 +94,23 @@ def get_statistics_language_hh(api_url, language):
     else:
         average_salary = 0
 
-    return create_dict_language(language, vacancies_found, vacancies_processed, average_salary)
+    return create_dict_language_statistics(language, vacancies_found, vacancies_processed, average_salary)
 
 
 def get_statistics_language_sj(api_url, secret_key, language):
     vacancies_found = 0
     all_salaries_vacancies = []
 
+    response = requests.get(api_url,
+                            headers=provide_headers_sj(secret_key),
+                            params=provide_params_sj(text=language))
+
+
     page = 0
     number_pages = 100
     while page < number_pages:
 
-        response = requests.get(api_url,
-                                headers=provide_headers_sj(secret_key),
-                                params=provide_params_sj(text=language))
+
         page += 100
         number_pages = response.json()['total']
         vacancies_found = response.json()['total']
@@ -126,37 +125,36 @@ def get_statistics_language_sj(api_url, secret_key, language):
     else:
         average_salary = 0
 
-    return create_dict_language(language, vacancies_found, vacancies_processed, average_salary)
+    return create_dict_language_statistics(language, vacancies_found, vacancies_processed, average_salary)
 
 
 def hh(programming_languages):
     api_url = 'https://api.hh.ru/vacancies?'
-    d = {}
+    dict_languages_statistics = {}
     for language in programming_languages:
-        d.update(get_statistics_language_hh(api_url, language))
+        dict_languages_statistics.update(get_statistics_language_hh(api_url, language))
 
-    return d
+    return dict_languages_statistics
 
 
-def superjob(secret_key, programming_languages=None):
+def superjob(secret_key, programming_languages):
     api_url = 'https://api.superjob.ru/2.0/vacancies/'
-    d = {}
+    dict_languages_statistics = {}
     for language in programming_languages:
-        d.update(get_statistics_language_sj(api_url, secret_key, language))
+        dict_languages_statistics.update(get_statistics_language_sj(api_url, secret_key, language))
 
-    return d
+    return dict_languages_statistics
 
 
-def create_table(title, statistics_languages):
+def create_table(title, dict_languages_statistics):
 
     table_data = [
         ['Язык программирования', 'Найдено вакансий', 'Обработано вакансий', 'Средняя зарплата']
     ]
-    for language in statistics_languages:
-        table_data.append([language] + list(statistics_languages[language].values()))
+    for language in dict_languages_statistics:
+        table_data.append([language] + list(dict_languages_statistics[language].values()))
 
     table = AsciiTable(table_data, title)
-
     print(table.table)
 
 
@@ -165,10 +163,8 @@ def main():
     secret_key = os.getenv('SECRET_KEY')
     programming_languages = ['Javascript', 'Java', 'Python', 'Ruby', 'Php', 'C++', 'C#', 'C', 'Go', 'Scala']
 
-    hh_d = hh(programming_languages)
-    sj_d = superjob(secret_key, programming_languages)
-    create_table('HeadHunter', hh_d)
-    create_table('SuperJob', sj_d)
+    create_table('HeadHunter', hh(programming_languages))
+    create_table('SuperJob', superjob(secret_key, programming_languages))
 
 
 if __name__ == '__main__':
